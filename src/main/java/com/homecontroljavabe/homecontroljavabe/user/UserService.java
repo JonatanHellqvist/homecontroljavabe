4d517fb685dfb62b46e970dfd15bb7242e2bd12b
JonatanHellqvist <143435050+JonatanHellqvist@users.noreply.github.com>
2024-10-25 16:01:56 +0200

    userLogin logik

--- src/main/java/com/homecontroljavabe/homecontroljavabe/user/UserService.java
+++ src/main/java/com/homecontroljavabe/homecontroljavabe/user/UserService.java
@@ -1,6 +1,9 @@
 package com.homecontroljavabe.homecontroljavabe.user;
 
+
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,4 +29,23 @@ public class UserService {
 		user.setPassword(passwordHash);
 		return mongoOperations.insert(user);
 	}
+
+	public boolean verifyPassword(String inputPassword, String storedHashedPassword) {
+        return BCrypt.checkpw(inputPassword, storedHashedPassword);
+    }
+
+	public User getUser(String id) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("id").is(id));
+		
+		return mongoOperations.findOne(query, User.class);
+	}
+
+	public User getUserByUsername(String username) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("username").is(username));
+		return mongoOperations.findOne(query, User.class);
+	}
+
+	
 }
